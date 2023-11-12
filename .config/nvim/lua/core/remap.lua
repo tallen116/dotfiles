cae84b5bedb1beb7a0244a19032879a850cca979
tallen116 <5997740+tallen116@users.noreply.github.com>
2023-11-12 17:38:42 -0500

    add remaps and sets

--- .config/nvim/lua/core/remap.lua
+++ .config/nvim/lua/core/remap.lua
@@ -1,2 +1,18 @@
 vim.g.mapleader = " "
+
+-- Project View / NetRW
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Keep cursor in the middle when jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Yank but keep current register
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- Easy escape
+vim.keymap.set("i", "<C-c>", "<Esc>")
