8a161895b99bd52b891e840d78c37e693a46f915
tallen116 <5997740+tallen116@users.noreply.github.com>
2023-11-12 17:39:14 -0500

    add QT QML highlighting

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "vim", "vimdoc" },
+			ensure_installed = { "lua", "vim", "vimdoc", "qmljs" },
 			auto_install = true,
 			highlight = {
 				enable = true,
